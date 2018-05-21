71ea58d42aee31a0d035e020f12d11b967e30b45
shizuku <xiao78050576@gmail.com>
2018-05-22 01:05:41 +0800

    初始提交

---
+++ src/main/java/com/shizuku/web/CanvasWsApplication.java
@@ -0,0 +1,12 @@
+package com.shizuku.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvasWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanvasWsApplication.class, args);
+	}
+}
