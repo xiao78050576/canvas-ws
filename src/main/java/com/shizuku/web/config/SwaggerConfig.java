2cfdfa565d75cbb1af7c656a566646b7e275f4a4
xiao78050576 <xiao78050576@gmail.com>
2018-09-29 01:04:20 +0800

    配置修改

--- src/main/java/com/shizuku/web/config/SwaggerConfig.java
+++ src/main/java/com/shizuku/web/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.shizuku.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -12,13 +13,13 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableWebMvc
 @EnableSwagger2
 public class SwaggerConfig {
 	@Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .groupName("shizuku")
-          .host("localhost:9090")
           .apiInfo(apiInfo())
           .select()                                  
           .apis(RequestHandlerSelectors.basePackage("com.shizuku.web.controller"))    
