08d407514cbe1d961e695258a29a468ab377da2c
shizuku <xiao78050576@gmail.com>
2019-01-27 23:12:44 +0800

    优化配置

--- src/main/java/com/shizuku/web/bean/SysUser.java
+++ src/main/java/com/shizuku/web/bean/SysUser.java
@@ -7,12 +7,17 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @TableName("sys_user")
 public class SysUser implements Serializable {
 
 	private static final long serialVersionUID = 3085443196443016310L;
 
-	@TableId(value="id",type=IdType.AUTO)
+	@TableId(value = "id", type = IdType.AUTO)
 	private Long id;
 
 	@TableField("name")
@@ -27,73 +32,4 @@ public class SysUser implements Serializable {
 	@TableField("comment")
 	private String comment;
 
-	/**
-	 * @return the id
-	 */
-	public Long getId() {
-		return id;
-	}
-
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/**
-	 * @return the username
-	 */
-	public String getUsername() {
-		return username;
-	}
-
-	/**
-	 * @param username the username to set
-	 */
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	/**
-	 * @return the password
-	 */
-	public String getPassword() {
-		return password;
-	}
-
-	/**
-	 * @param password the password to set
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	/**
-	 * @return the comment
-	 */
-	public String getComment() {
-		return comment;
-	}
-
-	/**
-	 * @param comment the comment to set
-	 */
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
 }
